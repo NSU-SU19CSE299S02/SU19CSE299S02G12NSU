9f8d279c2051cdc18c2566bed3d100a7dd8b7607
shanto <shan2neo@gmail.com>
2019-08-27 07:52:01 +0600

    Finishing homepage

--- Project Code/home.php
+++ Project Code/home.php
@@ -4,36 +4,37 @@
     
     }
 ?>
-<?php
+    <?php
  
   include 'library.php';
 ?>
-<!DOCTYPE html>
-<html lang="en">
+        <!DOCTYPE html>
+        <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="css/style.css">
-    <title>home page</title>
-</head>
+        <head>
+            <meta charset="UTF-8">
+            <link rel="stylesheet" href="css/style.css">
+            <title>home page</title>
+        </head>
 
-<body>
-    <div class="hero">
-        <div class="highway"></div>
-        <div class="city"></div>
-        <div class="car">
-            <img src="img/car.png" alt="a car">
-        </div>
-        <div class="wheel">
-            <img src="img/wheel.png"   class="back-wheel">
-            <img src="img/wheel.png"   class="front-wheel">
+        <body>
+            <div class="hero">
+                <div class="highway"></div>
+                <div class="city"></div>
+                <div class="car">
+                    <img src="img/car.png" alt="a car">
+                </div>
+                <div class="wheel">
+                    <img src="img/wheel.png" class="back-wheel">
+                    <img src="img/wheel.png" class="front-wheel">
 
 
-            </div>   
-             <div class="user_info">
-            <div class="u_dp"><img src="img/user-512.png" alt=""></div>
-            <h1 class="u_text">
- <?php
+                </div>
+                <div class="user_info">
+                    <div class="u_dp"><img src="img/user-512.png" alt=""></div>
+                    <h1 class="u_text">
+                      
+                            <?php
                  $emailu= $_SESSION['username'];
   $query ="SELECT fullname FROM `user` WHERE email='$emailu' ";
    $query_run=mysqli_query($connection,$query);
@@ -46,30 +47,14 @@ FROM friend_list f JOIN users_info ui on f.Friend_id=ui.email
 WHERE f.User_id ='$emailu' ";
                     $query_run=$connection->query($query);
 
-                    ?>      
-            
-            
-            </h1>
-            
-            
-            
-            <div class="u_his">
-            
-            
-            
-         <a href="#" class="mega_button" style="display: block;
-    background: red;
-    text-color: red;
-    color: white;
-    text-decoration: none;
-    padding: 5px;
-    border-radius: 14px;
-    font-family: -webkit-pictograph;
-    border: 2px solid #8b8b8b;">Buy Ticket</a>
-            </div>
+                    ?>
 
 
-            <div class="u_his">
+                    </h1>
+
+
+
+                    <div class="u_his">
 
 
 
@@ -81,10 +66,9 @@ WHERE f.User_id ='$emailu' ";
     padding: 5px;
     border-radius: 14px;
     font-family: -webkit-pictograph;
-    border: 2px solid #8b8b8b;">Balance $50</a>
+    border: 2px solid #8b8b8b;">Buy Ticket</a>
                     </div>
-
-                    <div class="u_his">
+                     <div class="u_his">
 
 
 
@@ -102,10 +86,26 @@ WHERE f.User_id ='$emailu' ";
                    
                    Log Out</a>
                     </div>
-            
-            
-            
-         <?php
+ <div class="u_his">
+
+
+
+                        <a href="#" class="mega_button" style="display: block;
+    background: red;
+    text-color: red;
+    color: white;
+    text-decoration: none;
+    padding: 5px;
+    border-radius: 14px;
+    font-family: -webkit-pictograph;
+    border: 2px solid #8b8b8b;">Balance $50</a>
+                    </div>
+
+
+
+
+                </div>
+                <?php
                 
                  
                  if(isset($_POST['Your_location'])){
@@ -127,15 +127,14 @@ WHERE ld.Location='$location' and ld.Destination= '$destination' ";
 
 $query_run2=$connection->query($sql2);
                  }
-                 ?>    
-        </div>
-        
-        
-         <div class="search_form">
-            <h1>Search Your Trip</h1>
-             <form action="">
-                <div class="location">
-            <input type="text" name="Your_location" placeholder="location" style="font-family: Poppins-Medium;
+                 ?>
+
+
+                    <div class="search_form" style="left:10%">
+                        <h1>Search Your Trip</h1>
+                        <form action="home.php" method="post">
+                            <div class="location">
+                                <input type="text" name="Your_location" placeholder="location " style="font-family: Poppins-Medium;
     font-size: 15px;
     line-height: 1.5;
     color: #666666;
@@ -145,9 +144,9 @@ $query_run2=$connection->query($sql2);
     height: 50px;
     border-radius: 25px;
     padding: 0 23px 0 81px;">
-                </div>
-                <div class="destination">
-            <input type="text" name="Your_destination" placeholder="Destination" style="font-family: Poppins-Medium;
+                            </div>
+                            <div class="destination">
+                                <input type="text" name="Your_destination" placeholder="Destination"  style="font-family: Poppins-Medium;
     font-size: 15px;
     line-height: 1.5;
     color: #666666;
@@ -157,21 +156,23 @@ $query_run2=$connection->query($sql2);
     height: 50px;
     border-radius: 25px;
     padding: 0 23px 0 81px;">
-                </div> 
-                <!-- <div class="date">
+                            </div>
+                            <!--
+                <div class="date">
             <input type="date" name="Your_tirp_date" placeholder="Date">
                
-                </div> -->
-                <div class="submit" >
-                  <input type="submit" value="search" style="border: none;
+                </div>
+-->
+                            <div class="submit" name="search"  >
+
+                                <input type="submit" value="search" style="border: none;
     background: white;
     border-radius: 10px;
     padding: 20px;">
-                </div>
-                 
-             </form>
+                            </div>
 
-             <?php
+                        </form>
+                        <?php
                         
               $count=mysqli_num_rows($query_run);
                      if($count == 0) {
@@ -202,12 +203,15 @@ $query_run2=$connection->query($sql2);
  text-align: left;
  padding: 8px;">' .$row['Description'].'</td>';
 
-
- echo' <td     style="border: 1px solid #dddddd;
+ 
+                        echo' <td     style="border: 1px solid #dddddd;
  text-align: left;
  padding: 8px;">' .$row['duration'].'</td>';
-
-  echo'<td>';
+                        
+                        
+                        
+                       
+                        echo'<td>';
                         while ($row2=$query_run2->fetch_array())
                         {
                            echo' 
@@ -218,7 +222,32 @@ $query_run2=$connection->query($sql2);
                         }
                         echo'</td>';
                     echo'</tr>';
-                         echo '<br><tr  style=":nth-child(even) {
+                        
+                            
+                         
+//                        <td>
+//                        
+//                         .$row['duration'].';
+//                        </td>
+//                        </tr>';
+//                        
+//                        echo '<td  style="border: 1px solid #dddddd;
+//  text-align: left;
+//  padding: 8px;">'.$row['Description'].'</td>';
+//            echo '<td style="border: 1px solid #dddddd;
+//  text-align: left;
+//  padding: 8px;">'.$row['duration'].'</td>'; </tr>
+//                  <td>      while ($row2=$query_run2->fetch_array())
+//  { 
+//                             echo '<td style="border: 1px solid #dddddd;
+//  text-align: left;
+//  padding: 8px;">'.$row['name'].'</td>';
+//      
+//  }
+//                        </td>    </tr>
+           
+        
+          echo '<br><tr  style=":nth-child(even) {
   background-color: #dddddd;
 }"><th style="border: 1px solid #dddddd;
   text-align: left;
@@ -232,46 +261,47 @@ $query_run2=$connection->query($sql2);
         
     } }?>
 
+                    </div>
 
-         </div>
+            </div>
+<!--
+            <nav>
+                <ul>
+                    <li>
+                        <div class="home-icon">
+                            <div class="roof">
+                                <div class="roo2f-edge"></div>
+                            </div>
+                            <div class="front"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="about-icon">
+                            <div class="head">
+                                <div class="eyes"></div>
+                                <div class="beard"></div>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="work-icon">
+                            <div class="paper"></div>
+                            <div class="lines"></div>
+                            <div class="lines"></div>
+                            <div class="lines"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="mail-icon">
+                            <div class="mail-base">
+                                <div class="mail-top"></div>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+-->
 
-        </div>
-    <nav>
-  <ul>
-    <li>
-      <div class="home-icon">
-        <div class="roof">
-          <div class="roo2f-edge"></div>
-        </div>
-        <div class="front"></div>
-      </div>
-    </li>
-    <li>
-      <div class="about-icon">
-        <div class="head">
-          <div class="eyes"></div>
-          <div class="beard"></div>
-        </div>
-      </div>
-    </li>
-    <li>
-      <div class="work-icon">
-        <div class="paper"></div>
-        <div class="lines"></div>
-        <div class="lines"></div>
-        <div class="lines"></div>
-      </div>
-    </li>
-    <li>
-      <div class="mail-icon">
-        <div class="mail-base">
-          <div class="mail-top"></div>
-        </div>
-      </div>
-    </li>
-  </ul>
-</nav>
+        </body>
 
-</body>
-
-</html>
+        </html>
